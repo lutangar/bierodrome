b331d3d11d82db7fff73fddf3841360b21e6b8ab
lutangar <johan.dufour@gmail.com>
2022-05-06 13:27:56 +0200

    chore: fix build

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,10 @@
-export const appendUnicodeFavicon = (unicode) =>
-  document.head.appendChild(
-    Object.assign(document.createElement('link'), {
-      rel: 'icon',
-      href: `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>${unicode}</text></svg>`,
-    })
-  );
+export const appendUnicodeFavicon = (unicode) => {
+  if (typeof window !== 'undefined') {
+    document.head.appendChild(
+      Object.assign(document.createElement('link'), {
+        rel: 'icon',
+        href: `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>${unicode}</text></svg>`,
+      })
+    );
+  }
+};
